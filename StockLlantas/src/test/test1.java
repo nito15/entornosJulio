51e5329f87ca15df1717ea970b6850e1a421afdb
nito15 <juanalepuz@hotmail.com>
2014-07-03 17:07:49 +0200

    Añadido test

---
+++ StockLlantas/src/test/test1.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class test1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
